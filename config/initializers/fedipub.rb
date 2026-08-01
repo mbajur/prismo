09b9f7ec0d1742d062eb80edc242562f7a1f39c4
mbajur <mbajur@gmail.com>
2026-08-01 14:34:26 +0200

    Prefix Fedipub site host with https scheme

--- config/initializers/fedipub.rb
+++ config/initializers/fedipub.rb
@@ -5,7 +5,7 @@ require "fedipub/data_transformer/note"
 Fedipub.config_from "fedipub"
 
 Fedipub.configure do |config|
-  config.site_host = ENV["HOST"]
+  config.site_host = "https://#{ENV['HOST']}"
   config.open_registrations = -> { Setting.open_registrations }
 end
 
