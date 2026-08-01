d6d63aa167a2cc846de43804a01b053eba2cc3cd
mbajur <mbajur@gmail.com>
2026-08-01 14:13:04 +0200

    Downgrade date-fns importmap pin to 4.1.0

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,7 +6,7 @@ pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@stimulus-components/timeago", to: "@stimulus-components--timeago.js" # @5.0.2
-pin "date-fns", to: "https://ga.jspm.io/npm:date-fns@4.4.0/index.js"
+pin "date-fns", to: "https://ga.jspm.io/npm:date-fns@4.1.0/index.js"
 pin "@floating-ui/dom", to: "@floating-ui--dom.js" # @1.8.0
 pin "@floating-ui/core", to: "@floating-ui--core.js" # @1.8.0
 pin "@floating-ui/utils", to: "@floating-ui--utils.js" # @0.2.12
