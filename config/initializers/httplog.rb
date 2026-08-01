8df971ae05455d4aa274e336cbbea9c90fea6d5c
mbajur <mbajur@gmail.com>
2026-08-01 09:21:25 +0200

    Brfore-production fixes

--- config/initializers/httplog.rb
+++ config/initializers/httplog.rb
@@ -1,9 +1,11 @@
-HttpLog.configure do |config|
-  # Enable or disable all logging
-  config.enabled = true
+if Rails.env.development?
+  HttpLog.configure do |config|
+    # Enable or disable all logging
+    config.enabled = true
 
-  # config.log_headers = true
+    # config.log_headers = true
 
-  # Log everything to log/http.log
-  # config.logger = Logger.new(Rails.root.join("log", "http.log"))
+    # Log everything to log/http.log
+    # config.logger = Logger.new(Rails.root.join("log", "http.log"))
+  end
 end
