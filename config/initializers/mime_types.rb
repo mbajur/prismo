a719ac0e03b95a55814b85359b76a8c784cada5b
mbajur <mbajur@gmail.com>
2026-08-12 22:41:01 +0200

    Disable plain application/ld+json ActivityPub mapping

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -4,4 +4,4 @@
 # 'application/ld+json; profile="https://www.w3.org/ns/activitystreams"' string.
 # Some clients send the bare media type without the profile parameter, so
 # accept that as :activitypub too.
-Mime::LOOKUP["application/ld+json"] = Mime[:activitypub]
+# Mime::LOOKUP["application/ld+json"] = Mime[:activitypub]
