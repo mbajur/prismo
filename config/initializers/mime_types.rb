458948e4d029a4ab92a67c712f862334b6238404
mbajur <mbajur@gmail.com>
2026-08-12 22:28:41 +0200

    Register bare application/ld+json as activitypub

---
+++ config/initializers/mime_types.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# The fedipub gem only registers :activitypub for the full
+# 'application/ld+json; profile="https://www.w3.org/ns/activitystreams"' string.
+# Some clients send the bare media type without the profile parameter, so
+# accept that as :activitypub too.
+Mime::LOOKUP["application/ld+json"] = Mime[:activitypub]
