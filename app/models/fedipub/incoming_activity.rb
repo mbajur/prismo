4c2662205aba5473d3409f4cd0cacdb04948a538
mbajur <mbajur@gmail.com>
2026-08-13 12:11:12 +0200

    Update incoming_activity.rb

--- app/models/fedipub/incoming_activity.rb
+++ app/models/fedipub/incoming_activity.rb
@@ -1,5 +1,10 @@
 class Fedipub::IncomingActivity < ApplicationRecord
-  enum :status, [ :pending, :processing, :processed, :failed ], default: :pending
+  enum :status, {
+    pending: "pending",
+    processing: "processing",
+    processed: "processed",
+    failed: "failed"
+  }, default: :pending
 
   validates :data, presence: true
   validates :entity_class, presence: true
